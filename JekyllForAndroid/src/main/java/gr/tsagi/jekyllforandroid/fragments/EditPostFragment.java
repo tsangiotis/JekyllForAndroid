19801e21c2ab195d675560d2c4905f0adb9ed0f0
Tasos Sangiotis <tsagiotis@gmail.com>
2014-08-15 13:01:43 +0300

    progress on edit post

--- JekyllForAndroid/src/main/java/gr/tsagi/jekyllforandroid/fragments/EditPostFragment.java
+++ JekyllForAndroid/src/main/java/gr/tsagi/jekyllforandroid/fragments/EditPostFragment.java
@@ -1,12 +1,12 @@
 package gr.tsagi.jekyllforandroid.fragments;
 
 import android.app.Fragment;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,15 +17,7 @@ import gr.tsagi.jekyllforandroid.activities.EditPostActivity;
 import gr.tsagi.jekyllforandroid.data.PostsContract.CategoryEntry;
 import gr.tsagi.jekyllforandroid.data.PostsContract.PostEntry;
 import gr.tsagi.jekyllforandroid.data.PostsContract.TagEntry;
-import gr.tsagi.jekyllforandroid.utils.Utility;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link EditPostFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- *
- */
 public class EditPostFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String LOG_TAG = EditPostFragment.class.getSimpleName();
@@ -87,14 +79,13 @@ public class EditPostFragment extends Fragment implements LoaderManager.LoaderCa
         // Sort order:  Ascending, by date.
         String sortOrder = PostEntry.COLUMN_DATETEXT + " DESC";
 
-        Uri weatherForLocationUri = PostEntry.buildWeatherLocationWithDate(
-                mLocation, mDateStr);
+        Uri postFromId = PostEntry.buildPostFromId(mPostId);
 
         // Now create and return a CursorLoader that will take care of
         // creating a Cursor for the data being displayed.
         return new CursorLoader(
                 getActivity(),
-                weatherForLocationUri,
+                postFromId,
                 POST_COLUMNS,
                 null,
                 null,
@@ -108,51 +99,15 @@ public class EditPostFragment extends Fragment implements LoaderManager.LoaderCa
             // Read weather condition ID from cursor
             String postId = data.getString(data.getColumnIndex(PostEntry.COLUMN_POST_ID));
             // Use weather art image
-            mTitle.setText(postId);
+
 
             // Read date from cursor and update views for day of week and date
-            String tag = data.getString(data.getColumnIndex(TagEntry.COLUMN_TAG));
-            
-            mFriendlyDateView.setText(friendlyDateText);
-            mDateView.setText(dateText);
+            String title = data.getString(data.getColumnIndex(PostEntry.COLUMN_TITLE));
+            String whereTag = TagEntry.COLUMN_POST_ID + " =?";
+            mTitle.setText(postId);
 
-            // Read description from cursor and update view
-            String description = data.getString(data.getColumnIndex(
-                    WeatherEntry.COLUMN_SHORT_DESC));
-            mDescriptionView.setText(description);
-
-            // Read high temperature from cursor and update view
-            boolean isMetric = Utility.isMetric(getActivity());
-
-            double high = data.getDouble(data.getColumnIndex(WeatherEntry.COLUMN_MAX_TEMP));
-            String highString = Utility.formatTemperature(getActivity(), high, isMetric);
-            mHighTempView.setText(highString);
-
-            // Read low temperature from cursor and update view
-            double low = data.getDouble(data.getColumnIndex(WeatherEntry.COLUMN_MIN_TEMP));
-            String lowString = Utility.formatTemperature(getActivity(), low, isMetric);
-            mLowTempView.setText(lowString);
-
-            // Read humidity from cursor and update view
-            float humidity = data.getFloat(data.getColumnIndex(WeatherEntry.COLUMN_HUMIDITY));
-            mHumidityView.setText(getActivity().getString(R.string.format_humidity, humidity));
-
-            // Read wind speed and direction from cursor and update view
-            float windSpeedStr = data.getFloat(data.getColumnIndex(WeatherEntry.COLUMN_WIND_SPEED));
-            float windDirStr = data.getFloat(data.getColumnIndex(WeatherEntry.COLUMN_DEGREES));
-            mWindView.setText(Utility.getFormattedWind(getActivity(), windSpeedStr, windDirStr));
-
-            // Read pressure from cursor and update view
-            float pressure = data.getFloat(data.getColumnIndex(WeatherEntry.COLUMN_PRESSURE));
-            mPressureView.setText(getActivity().getString(R.string.format_pressure, pressure));
-
-            // We still need this for the share intent
-            mForecast = String.format("%s - %s - %s/%s", dateText, description, high, low);
-
-            // If onCreateOptionsMenu has already happened, we need to update the share intent now.
-            if (mShareActionProvider != null) {
-                mShareActionProvider.setShareIntent(createShareForecastIntent(mForecast));
-            }
+            String content = data.getString(data.getColumnIndex(PostEntry.COLUMN_CONTENT));
+            mContent.setText(content);
         }
     }
 

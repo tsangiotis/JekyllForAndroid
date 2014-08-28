360f044acfd7ff7cb3d066191d32390929c5f810
Tasos Sangiotis <tsagiotis@gmail.com>
2014-08-28 11:34:05 +0300

    More directory changes

--- app/src/androidTest/java/gr/tsagi/jekyllforandroid/app/test/FullTestSuite.java
+++ app/src/androidTest/java/gr/tsagi/jekyllforandroid/app/test/FullTestSuite.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.sunshine.app.test;
+package gr.tsagi.jekyllforandroid.app.test;
 
 import android.test.suitebuilder.TestSuiteBuilder;
 

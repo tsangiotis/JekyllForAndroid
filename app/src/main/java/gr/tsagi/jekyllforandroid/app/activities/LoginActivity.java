e413102ec02df7f284c02f2f7189ddcf1acc0430
Tasos Sangiotis <tsagiotis@gmail.com>
2014-08-31 14:49:47 +0300

    updated .gitignore

--- app/src/main/java/gr/tsagi/jekyllforandroid/app/activities/LoginActivity.java
+++ app/src/main/java/gr/tsagi/jekyllforandroid/app/activities/LoginActivity.java
@@ -2,9 +2,11 @@ package gr.tsagi.jekyllforandroid.app.activities;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
@@ -12,22 +14,29 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
+import org.eclipse.egit.github.core.Authorization;
 import org.eclipse.egit.github.core.User;
 import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.OAuthService;
 import org.eclipse.egit.github.core.service.UserService;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import gr.tsagi.jekyllforandroid.app.R;
 import gr.tsagi.jekyllforandroid.app.utils.GetAccessToken;
@@ -201,5 +210,118 @@ public class LoginActivity extends Activity {
 
             finish();
         }
+    }
+
+    private void doLogin() {
+        EditText loginView = (EditText) findViewById(R.id.username);
+        EditText passwordView = (EditText) findViewById(R.id.password);
+
+        UiUtils.hideImeForView(loginView);
+        UiUtils.hideImeForView(passwordView);
+
+        String username = loginView.getText().toString();
+        String password = passwordView.getText().toString();
+
+//        if (!StringUtils.checkEmail(username)) {
+//            new LoginTask(username, password).execute();
+//        } else {
+//            Toast.makeText(LoginActivity.this,
+//                    getString(R.string.enter_username_toast), Toast.LENGTH_LONG).show();
+//        }
+    }
+
+    private class LoginTask extends ProgressDialogTask<Authorization> {
+        private String mUserName;
+        private String mPassword;
+        private String mOtpCode;
+
+        /**
+         * Instantiates a new load repository list task.
+         */
+        public LoginTask(String userName, String password) {
+            super(LoginActivity.this, R.string.please_wait, R.string.authenticating);
+            mUserName = userName;
+            mPassword = password;
+        }
+
+        public LoginTask(String userName, String password, String otpCode) {
+            super(LoginActivity.this, R.string.please_wait, R.string.authenticating);
+            mUserName = userName;
+            mPassword = password;
+            mOtpCode = otpCode;
+        }
+
+        @Override
+        protected Authorization run() throws IOException {
+            GitHubClient client = new ClientForAuthorization(mOtpCode);
+            client.setCredentials(mUserName, mPassword);
+            client.setUserAgent("Gh4a");
+
+            Authorization auth = null;
+            OAuthService authService = new OAuthService(client);
+            List<Authorization> auths = authService.getAuthorizations();
+            for (Authorization authorization : auths) {
+                if ("Gh4a".equals(authorization.getNote())) {
+                    auth = authorization;
+                    break;
+                }
+            }
+
+            if (auth == null) {
+                auth = new Authorization();
+                auth.setNote("Gh4a");
+                auth.setUrl("http://github.com/slapperwan/gh4a");
+                List<String> scopes = new ArrayList<String>();
+                scopes.add("user");
+                scopes.add("repo");
+                scopes.add("gist");
+                auth.setScopes(scopes);
+
+                auth = authService.createAuthorization(auth);
+            }
+            return auth;
+        }
+
+        @Override
+        protected void onError(Exception e) {
+            if (e instanceof TwoFactorAuthException) {
+                open2FADialog(mUserName, mPassword);
+            } else {
+                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        protected void onSuccess(Authorization result) {
+            SharedPreferences sharedPreferences = getSharedPreferences(
+                    SyncStateContract.Constants.PREF_NAME, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(Constants.User.AUTH_TOKEN, result.getToken());
+            editor.putString(Constants.User.LOGIN, mUserName);
+            editor.commit();
+
+            IntentUtils.openUserInfoActivity(this, mUserName,
+                    null, Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            finish();
+        }
+    }
+
+    private void open2FADialog(final String username, final String password) {
+        LayoutInflater inflater = LayoutInflater.from(LoginActivity.this);
+        View authDialog = inflater.inflate(R.layout.twofactor_auth_dialog, null);
+        final EditText authCode = (EditText) authDialog.findViewById(R.id.auth_code);
+
+        new AlertDialog.Builder(this)
+                .setCancelable(false)
+                .setTitle(R.string.two_factor_auth)
+                .setView(authDialog)
+                .setPositiveButton(R.string.verify, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new LoginTask(username, password, authCode.getText().toString()).execute();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
     }
 }

a5f3dc51220afa2206a4cbb66b9f37852e5dbf0e
jchanghong <3200601@qq.com>
2017-06-17 12:28:17 +0800

    redesign user interface

---
+++ app/src/main/java/com/jchanghong/test/main.kt
@@ -0,0 +1,11 @@
+package com.jchanghong.test
+
+/**
+\* Created with IntelliJ IDEA.
+\* User: jchanghong
+\* Date: 2017/6/17 0017
+\* Time: 12:19
+\*/
+fun main(args: Array<String>) {
+    println("helo")
+}

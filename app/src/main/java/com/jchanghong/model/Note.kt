48ac33486c767520c0d626e3a3464fb8da9f0106
jchanghong <3200601@qq.com>
2017-06-18 22:50:17 +0800

    update code

--- app/src/main/java/com/jchanghong/model/Note.kt
+++ app/src/main/java/com/jchanghong/model/Note.kt
@@ -2,28 +2,17 @@ package com.jchanghong.model
 
 import java.io.Serializable
 
-class Note : Serializable {
-    var id: Long = 0
-    var tittle: String? = null
-    var content: String? = null
-    var lastEdit: Long = 0
-    var favourite = 0
-    var category: Category? = null
-
-    constructor()
-
-    constructor(tittle: String, content: String, last_edit: Long, favourite: Int, category: Category) {
-        this.tittle = tittle
-        this.content = content
-        this.lastEdit = last_edit
-        this.favourite = favourite
-        this.category = category
-    }
+data class Note(var id: Long = 0,
+                var tittle: String="",
+                var content: String="",
+                var lastEdit: Long = 0,
+                var favourite: Int = 0,
+                var category: Category?=null) : Serializable {
 
     fun clear() {
         this.id = 0
-        this.tittle = null
-        this.content = null
+        this.tittle = ""
+        this.content = ""
         this.lastEdit = 0
         this.favourite = 0
     }

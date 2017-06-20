e4747fa9fb1f0e3277f7f6c52286c94532f0cbc3
jchanghong <3200601@qq.com>
2017-06-20 18:16:16 +0800

    base complete

--- app/src/main/java/gr/tsagi/jekyllforandroid/app/fragments/MarkdownPreviewFragment.java
+++ app/src/main/java/gr/tsagi/jekyllforandroid/app/fragments/MarkdownPreviewFragment.java
@@ -54,17 +54,18 @@ public class MarkdownPreviewFragment extends Fragment {
         webview.getSettings().setLoadWithOverviewMode(true);
         webview.getSettings().setUseWideViewPort(true);
         webview.setWebChromeClient(new WebChromeClient());
+        webview.setBackgroundColor(com.jchanghong.R.color.background_material_light);
 
         AndDown andDown = new AndDown();
         String htmlData = andDown.markdownToHtml(content);
 
-        htmlData = "<meta name=\"HandheldFriendly\" content=\"True\">\n" +
-                "<meta name=\"MobileOptimized\" content=\"320\">\n" +
-                "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"+
-                "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/main.min.css?family=Lato:300,400,700,300italic,400italic\" />" +
-                "<meta http-equiv=\"cleartype\" content=\"on\">"+
-                "<script src=\"js/vendor/modernizr-2.6.2.custom.min.js\"></script>"+
-                htmlData;
+//        htmlData = "<meta name=\"HandheldFriendly\" content=\"True\">\n" +
+//                "<meta name=\"MobileOptimized\" content=\"320\">\n" +
+//                "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"+
+//                "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/main.min.css?family=Lato:300,400,700,300italic,400italic\" />" +
+//                "<meta http-equiv=\"cleartype\" content=\"on\">"+
+//                "<script src=\"js/vendor/modernizr-2.6.2.custom.min.js\"></script>"+
+//                htmlData;
 
         webview.loadDataWithBaseURL("file:///android_asset/", htmlData, "text/html", "UTF-8", null);
 

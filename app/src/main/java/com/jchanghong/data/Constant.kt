6e794eb1dafb066905f007d8da52b21d5e90545e
jchanghong <3200601@qq.com>
2017-06-20 12:16:47 +0800

    update github code

--- app/src/main/java/com/jchanghong/data/Constant.kt
+++ app/src/main/java/com/jchanghong/data/Constant.kt
@@ -1,12 +1,10 @@
 package com.jchanghong.data
 
-import android.content.Context
 import java.util.*
 
 object Constant {
 
  const   val DEFAULT_CATEGORY_ID = 1
-    var mcontext:Context?=null
     val iszhong = Locale.getDefault().displayLanguage == "中文"
 
 }

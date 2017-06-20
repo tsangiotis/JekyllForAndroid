71a858e6e5f1e70c09f8afe372b72020cc316774
jchanghong <3200601@qq.com>
2017-06-20 16:43:18 +0800

    update github code

---
+++ app/src/main/java/com/jchanghong/utils/Strings.kt
@@ -0,0 +1,12 @@
+package com.jchanghong.utils
+
+
+/**
+\* Created with IntelliJ IDEA.
+\* User: jchanghong
+\* Date: 2017/6/20 0020
+\* Time: 16:41
+\*/
+fun String.hasYamHead():Boolean  {
+    return true
+}
---
+++ app/src/test/java/com/jchanghong/utils/StringsKtTest.kt
@@ -0,0 +1,19 @@
+package com.jchanghong.utils
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: jchanghong
+ * \* Date: 2017/6/20 0020
+ * \* Time: 16:42
+ * \
+ */
+class StringsKtTest {
+    @Test
+    fun hasYamHead() {
+        assertEquals(true,"".hasYamHead())
+    }
+
+}

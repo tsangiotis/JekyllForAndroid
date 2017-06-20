72ac309a3b2f43daefb614710b6bac4c7da03249
jchanghong <3200601@qq.com>
2017-06-20 18:45:58 +0800

    change markdown lib

--- app/src/main/java/gr/tsagi/jekyllforandroid/app/fragments/MarkdownPreviewFragment.java
+++ app/src/main/java/gr/tsagi/jekyllforandroid/app/fragments/MarkdownPreviewFragment.java
@@ -5,12 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
 
-import com.commonsware.cwac.anddown.AndDown;
-
+import br.tiagohm.markdownview.MarkdownView;
+import br.tiagohm.markdownview.css.styles.Github;
 import gr.tsagi.jekyllforandroid.app.R;
 import gr.tsagi.jekyllforandroid.app.activities.PreviewMarkdownActivity;
 import gr.tsagi.jekyllforandroid.app.utils.Utility;
@@ -45,28 +42,34 @@ public class MarkdownPreviewFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater
                 .inflate(R.layout.fragment_markdown_preview, container, false);
-
-        // Find the webview
-        WebView webview = (WebView) rootView.findViewById(R.id.markdown_preview_view);
-
-        WebSettings settings = webview.getSettings();
-        webview.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-        webview.getSettings().setLoadWithOverviewMode(true);
-        webview.getSettings().setUseWideViewPort(true);
-        webview.setWebChromeClient(new WebChromeClient());
-
-        AndDown andDown = new AndDown();
-        String htmlData = andDown.markdownToHtml(content);
-
-        htmlData = "<meta name=\"HandheldFriendly\" content=\"True\">\n" +
-                "<meta name=\"MobileOptimized\" content=\"320\">\n" +
-                "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"+
-                "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/main.min.css?family=Lato:300,400,700,300italic,400italic\" />" +
-                "<meta http-equiv=\"cleartype\" content=\"on\">"+
-                "<script src=\"js/vendor/modernizr-2.6.2.custom.min.js\"></script>"+
-                htmlData;
-
-        webview.loadDataWithBaseURL("file:///android_asset/", htmlData, "text/html", "UTF-8", null);
+        MarkdownView mMarkdownView;
+        mMarkdownView = rootView.findViewById(R.id.markdown_preview_view);
+        mMarkdownView.addStyleSheet(new Github());
+        mMarkdownView.loadMarkdown(content);
+//        mMarkdownView.loadMarkdownFromAsset("markdown1.md");
+//        mMarkdownView.loadMarkdownFromFile(new File());
+//        mMarkdownView.loadMarkdownFromUrl("url");
+//        // Find the webview
+//        WebView webview = (WebView) rootView.findViewById(R.id.markdown_preview_view);
+//
+//        WebSettings settings = webview.getSettings();
+//        webview.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+//        webview.getSettings().setLoadWithOverviewMode(true);
+//        webview.getSettings().setUseWideViewPort(true);
+//        webview.setWebChromeClient(new WebChromeClient());
+//
+//        AndDown andDown = new AndDown();
+//        String htmlData = andDown.markdownToHtml(content);
+//
+//        htmlData = "<meta name=\"HandheldFriendly\" content=\"True\">\n" +
+//                "<meta name=\"MobileOptimized\" content=\"320\">\n" +
+//                "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"+
+//                "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/main.min.css?family=Lato:300,400,700,300italic,400italic\" />" +
+//                "<meta http-equiv=\"cleartype\" content=\"on\">"+
+//                "<script src=\"js/vendor/modernizr-2.6.2.custom.min.js\"></script>"+
+//                htmlData;
+//
+//        webview.loadDataWithBaseURL("file:///android_asset/", htmlData, "text/html", "UTF-8", null);
 
         return rootView;
     }

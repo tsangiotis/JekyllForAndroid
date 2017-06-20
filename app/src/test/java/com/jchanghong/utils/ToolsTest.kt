11dbd65f91364545fa9773feeb5ffe26e1d5c384
jchanghong <3200601@qq.com>
2017-06-20 16:41:31 +0800

    update github code

---
+++ app/src/test/java/com/jchanghong/utils/ToolsTest.kt
@@ -0,0 +1,18 @@
+package com.jchanghong.utils
+
+import org.junit.Test
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: jchanghong
+ * \* Date: 2017/6/20 0020
+ * \* Time: 16:39
+ * \
+ */
+class ToolsTest {
+    @Test
+    fun systemBarLolipop() {
+        println("test")
+    }
+
+}
